3ff39f09baf4cb435af628b3da377d8e75d5ffb7
Liang Liu <nlprliu@gmail.com>
2016-07-10 20:40:43 -0400

    Support getting current weather.

--- bin/getForecast.sh
+++ bin/getForecast.sh
@@ -7,4 +7,10 @@ DIR=$(dirname "$script")
 
 id="$1"
 forecast_log="$DIR/../data/forecast${id}.log"
-curl -s "http://api.openweathermap.org/data/2.5/forecast?id=$id&lang=zh&units=metric&appid=$api_key" |  jq '.list[]|{"dt":.dt, "temp":.main.temp, "info":.weather[].description,"wind_s":.wind.speed,"wind_d":.wind.deg}' | paste -sd '      \n' - > "$forecast_log"
+weather_log="$DIR/../data/weather${id}.log"
+url_prefix="http://api.openweathermap.org/data/2.5/"
+params="id=$id&lang=zh&units=metric&appid=$api_key"
+json_format='{"dt":.dt, "temp":.main.temp, "info":.weather[].description,"wind_s":.wind.speed,"wind_d":.wind.deg}'
+curl -s "${url_prefix}weather?$params" |  jq '.' > "$weather_log"
+cat "$weather_log" | jq "$json_format" | paste -sd '      \n' - > "$forecast_log"
+curl -s "${url_prefix}forecast?$params" |  jq ".list[]|$json_format" | paste -sd '      \n' - >> "$forecast_log"
